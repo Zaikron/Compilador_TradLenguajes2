b667cb1aa8d25803ad5c02b373bceacedf1e11f3
Diego Ivan <diegoivan1987@outlook.com>
2022-03-27 13:14:58 -0600

    reglas semanticas basicas todos los tipos de dato

--- Compilador/src/Code/SemanticAnalysis.java
+++ Compilador/src/Code/SemanticAnalysis.java
@@ -32,7 +32,7 @@ public class SemanticAnalysis {
         
         showStructs();
     }
-    
+    //dejar las declaracion simple al final de cada grupo de reglas
     public void structAnalysis(Production p, ArrayList<ErrorLSSL> errors, String struct){
         ArrayList<Token> tokens = p.getTokens();
         for(int i = 0; i < tokens.size(); i++){
@@ -43,83 +43,182 @@ public class SemanticAnalysis {
             //Variables byte
             //declaracion con asignacion
             if(t.getLexeme().equals("byte") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
+                //correcta
                 if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && Integer.parseInt(tokens.get(i+3).getLexicalComp())>0 && Integer.parseInt(tokens.get(i+3).getLexicalComp())<255){
                     addIdentifier(struct, tokens.get(i+1).getLexeme()); 
-                }else{
+                }else{//errores
+                    //fuera de rango
                     if(Integer.parseInt(tokens.get(i+3).getLexicalComp())< 0 && Integer.parseInt(tokens.get(i+3).getLexicalComp())> 255)
                     {
                         errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor fuera de rango(0-255)  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
                     }
+                    //tipo de dato incorrecto
                     else
                     {
                         errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
                     }
                 }
-            //declaracion
-            }else if(t.getLexeme().equals("byte") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
-                addIdentifier(struct, tokens.get(i+1).getLexeme());
             //asignacion de una operacion aritmetica
             }else if(t.getLexeme().equals("byte") && tokens.get(i+4).getLexicalComp().equals("OP_ARIT")){
+                //correcto
                 if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && tokens.get(i+5).getLexicalComp().equals("NUMERO")){
                     addIdentifier(struct, tokens.get(i+1).getLexeme()); 
-                }else{
-                    if((Integer.parseInt(tokens.get(i+3).getLexeme()) + Integer.parseInt(tokens.get(i+5).getLexeme()))>255 || 
-                       (Integer.parseInt(tokens.get(i+3).getLexeme()) - Integer.parseInt(tokens.get(i+5).getLexeme()) < 0) || 
-                       (Integer.parseInt(tokens.get(i+3).getLexeme()) * Integer.parseInt(tokens.get(i+5).getLexeme()) > 255)){
-                        errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Valor fuera de rango(0-255)  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
-                    }
-                    else
-                    {
-                        errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Operador no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
-                    }
-                }
-            }
-            
-            //Variables int
-            //asignacion
-            if(t.getLexeme().equals("int") && tokens.get(i+1).getLexicalComp().equals("ASIGNACION")){
-                if(tokens.get(i+3).getLexicalComp().equals("NUMERO")){
-                    addIdentifier(struct, tokens.get(i+1).getLexeme()); //Se agrega el identificador. En i es int y en i+1 es el nombre de la variable
-                }else{
-                    errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
-                }
-            //declaracion
-            }else if(t.getLexeme().equals("int") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
-                addIdentifier(struct, tokens.get(i+1).getLexeme());
-            //asignacion de una operacion aritmetica
-            }else if(t.getLexeme().equals("int") && tokens.get(i+4).getLexicalComp().equals("OP_ARIT")){
-                if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && tokens.get(i+5).getLexicalComp().equals("NUMERO")){
-                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato incorrecto en alguno de los operandos
                 }else{
                     errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Operador no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
                 }
-            }
-            
-            //Variables string
-            if(t.getLexeme().equals("string") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
-                if(tokens.get(i+4).getLexicalComp().equals("IDENTIFICADOR")){
-                    addIdentifier(struct, tokens.get(i+1).getLexeme()); //Se agrega el identificador. En i es int y en i+1 es el nombre de la variable
-                }else{
-                    errors.add(new ErrorLSSL(2, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
-                }
-            }else if(t.getLexeme().equals("string") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
+            //declaracion simple    
+            }else if(t.getLexeme().equals("byte") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
                 addIdentifier(struct, tokens.get(i+1).getLexeme());
             }
             
             //Variables char
+            //declaracion con asignacion
             if(t.getLexeme().equals("char") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
-                if(tokens.get(i+4).getLexicalComp().equals("IDENTIFICADOR")){
-                    addIdentifier(struct, tokens.get(i+1).getLexeme()); //Se agrega el identificador. En i es int y en i+1 es el nombre de la variable
+                //correcto
+                if(tokens.get(i+4).getLexicalComp().equals("IDENTIFICADOR") && tokens.get(i+4).getLexeme().length() == 1){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme());
+                //tipo de dato incorrecto
                 }else{
                     errors.add(new ErrorLSSL(3, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
                 }
+            //declaracion con concatenacion
+            }else if(t.getLexeme().equals("char") && tokens.get(i+6).getLexeme().equals("+")){
+                //correcta
+                if(tokens.get(i+4).getLexicalComp().equals("IDENTIFICADOR") && tokens.get(i+8).getLexicalComp().equals("IDENTIFICADOR") && tokens.get(i+4).getLexeme().length() == 1 && tokens.get(i+8).getLexeme().length() == 1){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme());
+                //uno de los operadores es un tipo de dato erroneo
+                }else{
+                    errors.add(new ErrorLSSL(3, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //declaracion simple
             }else if(t.getLexeme().equals("char") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
                 addIdentifier(struct, tokens.get(i+1).getLexeme());
             }
             
+            //variables long
+            //declaracion y asignacion
+            if(t.getLexeme().equals("long") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato incorrecto
+                }else{
+                    errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //asignacion de una operacion aritmetica
+            }else if(t.getLexeme().equals("long") && tokens.get(i+4).getLexicalComp().equals("OP_ARIT")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && tokens.get(i+5).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato erroneo
+                }else{
+                    errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Operador no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            }
+            //declaracion
+            else if(t.getLexeme().equals("long") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
+                addIdentifier(struct, tokens.get(i+1).getLexeme());
+            }
+            
+            //variable float
+            //declaracion y asignacion
+            if(t.getLexeme().equals("float") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato incorrecto
+                }else{
+                    errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //asignacion de una operacion aritmetica
+            }else if(t.getLexeme().equals("float") && tokens.get(i+4).getLexicalComp().equals("OP_ARIT")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && tokens.get(i+5).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato erroneo
+                }else{
+                    errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Operador no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            }
+            //declaracion
+            else if(t.getLexeme().equals("float") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
+                addIdentifier(struct, tokens.get(i+1).getLexeme());
+            }
+            
+            //variable double
+            //declaracion y asignacion
+            if(t.getLexeme().equals("double") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato incorrecto
+                }else{
+                    errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //asignacion de una operacion aritmetica
+            }else if(t.getLexeme().equals("double") && tokens.get(i+4).getLexicalComp().equals("OP_ARIT")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && tokens.get(i+5).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato erroneo
+                }else{
+                    errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Operador no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            }
+            //declaracion
+            else if(t.getLexeme().equals("double") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
+                addIdentifier(struct, tokens.get(i+1).getLexeme());
+            }
             
             
+            //Variables int
+            //declaracion y asignacion
+            if(t.getLexeme().equals("int") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato incorrecto
+                }else{
+                    errors.add(new ErrorLSSL(1, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //asignacion de una operacion aritmetica
+            }else if(t.getLexeme().equals("int") && tokens.get(i+4).getLexicalComp().equals("OP_ARIT")){
+                //correcta
+                if(tokens.get(i+3).getLexicalComp().equals("NUMERO") && tokens.get(i+5).getLexicalComp().equals("NUMERO")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                //tipo de dato erroneo
+                }else{
+                    errors.add(new ErrorLSSL(11, " --- Error Semantico({}): Operador no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            }
+            //declaracion
+            else if(t.getLexeme().equals("int") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
+                addIdentifier(struct, tokens.get(i+1).getLexeme());
+            }
             
+            //Variables string
+            //declaracion con asignacion
+            if(t.getLexeme().equals("string") && tokens.get(i+2).getLexicalComp().equals("ASIGNACION")){
+                //correcto
+                if(tokens.get(i+4).getLexicalComp().equals("IDENTIFICADOR")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme()); 
+                }else{
+                    errors.add(new ErrorLSSL(2, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //declaracion con concatenacion
+            }else if(t.getLexeme().equals("string") && tokens.get(i+6).getLexeme().equals("+")){
+                //correcta
+                if(tokens.get(i+4).getLexicalComp().equals("IDENTIFICADOR") && tokens.get(i+8).getLexicalComp().equals("IDENTIFICADOR")){
+                    addIdentifier(struct, tokens.get(i+1).getLexeme());
+                //uno de los operadores es un tipo de dato erroneo
+                }else{
+                    errors.add(new ErrorLSSL(3, " --- Error Semantico({}): Valor no compatible con el tipo de dato  [Linea: "+t.getLine()+", Caracter: "+t.getColumn()+"]", p, true));
+                }
+            //declaracion simple
+            }else if(t.getLexeme().equals("string") && tokens.get(i+1).getLexicalComp().equals("IDENTIFICADOR")){
+                addIdentifier(struct, tokens.get(i+1).getLexeme());
+            }
         }
     }
     
